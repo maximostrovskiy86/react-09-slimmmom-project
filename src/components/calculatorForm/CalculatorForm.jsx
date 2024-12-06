8a88f6fe6e1e7f52884c4030ccbab5d02fec109b
maximostrovskiy86 <maxim.ostrovskiy86@gmail.com>
2024-12-06 07:25:03 +0100

    feat: created a daily calorie intake request, added axios

--- src/components/calculatorForm/CalculatorForm.jsx
+++ src/components/calculatorForm/CalculatorForm.jsx
@@ -1,49 +1,122 @@
-import React, {useState} from "react";
+import React, {useState,} from "react";
+import {useDispatch} from "react-redux";
+import {fetchDailyCalorieIntake} from "../../redux/dailyCalorieIntake/dailyCalorieIntake-operations";
 import {CalculatorFormContainer, CalculateGroupContainer, FormRadioGroup, InputBox} from "./CalculatorForm.styled";
 import Button from "../button";
 
-const CalculatorForm = ({}) => {
+
+const CalculatorForm = () => {
+	const [height, setHeight] = useState('');
+	const [age, setAge] = useState('');
+	const [weight, setWeight] = useState('');
+	const [desiredWeight, setDesiredWeight] = useState('');
+	const [bloodType, setBloodType] = useState(1);
+	const dispatch = useDispatch();
+	
+	
+	const onHandleChange = e => {
+		const {value, name} = e.target;
+		
+		switch (name) {
+			case 'height':
+				setHeight(value);
+				break;
+			case 'age':
+				setAge(value);
+				break;
+			case 'weight':
+				setWeight(value);
+				break;
+			case 'desiredWeight':
+				setDesiredWeight(value);
+				break;
+			case 'bloodType':
+				setBloodType(value);
+				break;
+			default:
+				return;
+		}
+	}
+	
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		
+		dispatch(fetchDailyCalorieIntake({
+			weight,
+			height,
+			age,
+			desiredWeight,
+			bloodType
+		}))
+	}
+	
 	return (
-		<CalculatorFormContainer>
+		<CalculatorFormContainer onSubmit={handleSubmit}>
 			<CalculateGroupContainer role="group" aria-labelledby="contact-details-head">
 				<InputBox>
-					<input id="heightId" type="number" name="height" pattern="^[ 0-9]+$" required/>
+					<input
+						id="heightId"
+						type="number"
+						name="height"
+						pattern="^[ 0-9]+$"
+						required
+						value={height}
+						onChange={onHandleChange}/>
 					<label htmlFor="heightId">Height </label>
 				</InputBox>
 				<InputBox>
-					<input id="ageId" type="number" name="age" required/>
+					<input
+						id="ageId"
+						type="number"
+						name="age"
+						required
+						value={age}
+						onChange={onHandleChange}
+					/>
 					<label htmlFor="ageId">Age</label>
 				</InputBox>
 				<InputBox>
-					<input id="CurrentAgeId" type="number" name="current-age" minLength="2" maxLength="3" required/>
-					<label htmlFor="CurrentAgeId">Current age</label>
+					<input
+						id="weight"
+						type="number"
+						name="weight"
+						minLength="2"
+						maxLength="3"
+						required
+						value={weight}
+						onChange={onHandleChange}
+					/>
+					<label htmlFor="weight">Weight</label>
 				</InputBox>
 				<InputBox>
-					<input id="desiredWeightId" type="number" name="desired-weight"/>
+					<input
+						id="desiredWeightId"
+						type="number"
+						name="desiredWeight"
+						required
+						value={desiredWeight}
+						onChange={onHandleChange}
+					/>
 					<label htmlFor="desiredWeightId">Desired weight</label>
-				</InputBox>
-				<InputBox>
-					<input id="desiredWeightid" type="number" name="desired-weight"/>
-					<label htmlFor="desiredWeightid">Desired weight</label>
 				</InputBox>
 			</CalculateGroupContainer>
 			
 			<FormRadioGroup>
 				<legend className="form-radio-group-title">Blood group</legend>
 				<div className="radio-form-field">
-					<input type="radio" name="blood-group" value="1" id="first-blood-group"/>
+					<input type="radio" name="bloodType" value="1" id="first-blood-group"/>
 					<label className="form-label" htmlFor="first-blood-group">1</label>
 				</div>
 				<div className="radio-form-field">
-					<input type="radio" name="blood-group" value="2" id="second-blood-group"/>
+					<input type="radio" name="bloodType" value="2" id="second-blood-group"/>
 					<label className="form-label" htmlFor="second-blood-group">2</label>
 				</div>
 				<div className="radio-form-field">
-					<input type="radio" name="blood-group" value="3" id="third-blood-group"/>
+					<input type="radio" name="bloodType" value="3" id="third-blood-group"/>
 					<label className="form-label" htmlFor="third-blood-group">3</label>
 				</div>
 				<div className="radio-form-field">
-					<input type="radio" name="blood-group" value="4" id="fourth-blood-group"/>
+					<input type="radio" name="bloodType" value="4" id="fourth-blood-group"/>
 					<label className="form-label" htmlFor="fourth-blood-group">4</label>
 				</div>
 			</FormRadioGroup>
