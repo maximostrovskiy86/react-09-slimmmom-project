6a9e30485d207c73392bdffbb1683badad9dbc3f
maximostrovskiy86 <maxim.ostrovskiy86@gmail.com>
2024-12-09 14:44:29 +0100

    chore: add lint script

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
+import {ThemeProvider} from '@emotion/react'
+
 import store from './redux/store';
 import './index.css';
 import App from './components/App';
@@ -11,7 +13,9 @@ root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
-				<App/>
+				<ThemeProvider>
+					<App/>
+				</ThemeProvider>
 			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
